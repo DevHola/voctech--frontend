6f71f4515d83e6f8f22e5bdf8ec13f35c8727e0f
DevHola <Connectola@yahoo.com>
2022-11-10 07:40:41 +0100

    centralized css

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const bar = document.getElementById("bar");
 const bar1 = document.getElementById("bar1");
 const bar2 = document.getElementById("bar2");
-const sidebar = document.getElementById("sidebar");
+const bar3 = document.getElementById("bar11");
+const bar4 = document.getElementById("bar22");
+const bar5 = document.getElementById("bar111");
+const bar6 = document.getElementById("bar222");
+const sidebar = document.getElementById("sidebar-admin");
+const sidebar1 = document.getElementById("sidebar-artisan");
+const sidebar2 = document.getElementById("sidebar-user");
 const close = document.getElementById("close");
-const clos = document.getElementById("close1");
-const close2 = document.getElementById("close2");
+const clos = document.getElementById("close1-admin");
+const close2 = document.getElementById("close2-admin");
+const close3 = document.getElementById("close1-artisan");
+const close4 = document.getElementById("close2-artisan");
+const close5 = document.getElementById("close1-user");
+const close6 = document.getElementById("close2-user");
 const nav = document.getElementById("navigation");
-const headina = document.getElementById("headina");
+const headina = document.getElementById("headina-admin");
+const headinaartisan = document.getElementById("headina-artisan");
+const headinauser = document.getElementById("headina-user");
 const visio = document.getElementById("visio");
 const visioclose = document.getElementById("visioclose");
 const visio1 = document.getElementById("visio1");
@@ -22,11 +34,32 @@ if (bar) {
 if (bar1) {
   bar1.addEventListener("click", () => {
     headina.classList.add("active");
+    headinaartisan.classList.add("active");
   });
 }
 if (bar2) {
   bar2.addEventListener("click", () => {
     sidebar.classList.add("active");
+  });
+}
+if (bar3) {
+  bar3.addEventListener("click", () => {
+    headinaartisan.classList.add("active");
+  });
+}
+if (bar4) {
+  bar4.addEventListener("click", () => {
+    sidebar1.classList.add("active");
+  });
+}
+if (bar5) {
+  bar5.addEventListener("click", () => {
+    headinauser.classList.add("active");
+  });
+}
+if (bar6) {
+  bar6.addEventListener("click", () => {
+    sidebar2.classList.add("active");
   });
 }
 
@@ -52,6 +85,26 @@ if (clos) {
 if (close2) {
   close2.addEventListener("click", () => {
     sidebar.classList.remove("active");
+  });
+}
+if (close3) {
+  close3.addEventListener("click", () => {
+    headinaartisan.classList.remove("active");
+  });
+}
+if (close4) {
+  close4.addEventListener("click", () => {
+    sidebar1.classList.remove("active");
+  });
+}
+if (close5) {
+  close5.addEventListener("click", () => {
+    headinauser.classList.remove("active");
+  });
+}
+if (close6) {
+  close6.addEventListener("click", () => {
+    sidebar2.classList.remove("active");
   });
 }
 if (popup.style.visibility === "visible") {
